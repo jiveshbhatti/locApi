2ede7467e4070e71ef36c2b5cb6e17cc06926f9f
Jivesh <85315081+jiveshbhatti@users.noreply.github.com>
2022-02-11 18:20:13 -0800

    Delete api directory

--- api/routes/apiv0Routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const res = require("express/lib/response");
-const router = express.Router();
-const { getTokenBalance } = require("../controllers/apiv0Routes");
-
-router.get("/:erc/:token", getTokenBalance);
-router.get("*", (req, res) => res.status(404).send("not found"));
-
-module.exports = router;
